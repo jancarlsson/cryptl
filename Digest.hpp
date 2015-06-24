77eeb973691513d64cb33cc30245a34be4b3774a
jancarlsson <jancarlsson@Safe-mail.net>
2015-06-24 01:02:19 +0000

    bless pre-image with lambda

--- Digest.hpp
+++ Digest.hpp
@@ -3,6 +3,7 @@
 
 #include <climits>
 #include <cstdint>
+#include <functional>
 #include <istream>
 #include <sstream>
 #include <vector>
@@ -17,10 +18,14 @@ namespace cryptl {
 
 // consumes the entire stream which is presumed to be properly padded
 template <typename T>
-typename T::DigType digest(T hashAlgo, std::istream& is) {
+typename T::DigType digest(
+    T hashAlgo,
+    std::istream& is,
+    std::function<bool (typename T::WordType&, std::istream&)> func)
+{
     typename T::MsgType msg;
 
-    while (!is.eof() && bless(msg, is)) {
+    while (!is.eof() && bless(msg, is, func)) {
         hashAlgo.msgInput(msg);
     }
 
@@ -28,16 +33,33 @@ typename T::DigType digest(T hashAlgo, std::istream& is) {
     return hashAlgo.digest();
 }
 
+template <typename T>
+typename T::DigType digest(T hashAlgo, std::istream& is)
+{
+    return digest(
+        hashAlgo,
+        is,
+        [] (typename T::WordType& a, std::istream& is) {
+            return bless(a, is);
+        });
+}
+
 // pads the byte vector message
 template <typename T>
-typename T::DigType digest(T hashAlgo, const std::vector<std::uint8_t>& a) {
+typename T::DigType digest(T hashAlgo, const std::vector<std::uint8_t>& a)
+{
     std::stringstream ss;
     for (const auto& b : a) ss.put(b);
 
     std::size_t lengthBits = a.size() * CHAR_BIT;
     T::padMessage(ss, lengthBits);
 
-    return digest(hashAlgo, ss);
+    return digest(
+        hashAlgo,
+        ss,
+        [] (typename T::WordType& a, std::istream& is) {
+            return bless(a, is);
+        });
 }
 
 } // namespace cryptl
